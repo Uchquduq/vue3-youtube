6dcbead07f702b7fe58449c23a8201a978c8795a
Rufat <rufatxasanov1998@gmail.com>
2023-04-23 18:02:14 +0500

    youtube

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,10 @@
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import 'vuetify/lib/styles/main.css'
+import * as components from 'vuetify/lib/components'
+import * as directives from 'vuetify/lib/directives'
+
+export default createVuetify({
+  components,
+  directives,
+})
